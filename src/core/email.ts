2f557c4b9a625e1852ffa31f89f0269d8fdca736
Alejandro Perdomo <apergot95@gmail.com>
2024-03-23 20:13:24 +0000

    Feat - created email value object

---
+++ src/core/email.ts
@@ -0,0 +1,29 @@
+import {ValidationError} from "./validationError";
+
+export class Email {
+
+    public static INVALID_FORMAT_ERROR_MSG = 'Invalid email format';
+
+    private constructor(
+        private address: string
+    ) {
+    }
+
+    static create(address: string) {
+        this.isValidAddress(address);
+        return new Email(address);
+    }
+
+    private static isValidAddress(address: string) {
+        const regex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+        if (!regex.test(address)) throw new ValidationError(Email.INVALID_FORMAT_ERROR_MSG)
+    }
+
+    toString() {
+        return this.address;
+    }
+
+    equals(email: Email) {
+        return email.address === this.address
+    }
+}
---
+++ src/tests/unit/core/email.test.ts
@@ -0,0 +1,30 @@
+import {Email} from "../../../core/email";
+
+describe('The Email', () => {
+    it('creates an email for a given address in a correct format',  () => {
+        const address = 'example@example.com';
+        const email = Email.create(address);
+
+        expect(email.toString()).toEqual('example@example.com');
+    });
+
+    it('does not allow addresses with wrong format', () => {
+        expect(() => {
+            Email.create('invalid')
+        }).toThrow(Email.INVALID_FORMAT_ERROR_MSG);
+    });
+
+    it('considers two emails with same address are the same', () => {
+        const email1 = Email.create('example@example.com');
+        const email2 = Email.create('example@example.com');
+
+        expect(email1.equals(email2)).toEqual(true);
+    });
+
+    it('considers two emails with different address are not the same', () => {
+        const email1 = Email.create('example@example.com');
+        const email2 = Email.create('example2@example.com');
+
+        expect(email1.equals(email2)).toEqual(false);
+    });
+});
